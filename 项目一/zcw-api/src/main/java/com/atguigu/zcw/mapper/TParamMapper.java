fa01aa855a6c9d702e65d49ef82f16af5505b5f4
rex_yan <rex_yan@126.com>
2020-03-14 22:49:47 +0800

    up

---
+++ 项目一/zcw-api/src/main/java/com/atguigu/zcw/mapper/TParamMapper.java
@@ -0,0 +1,30 @@
+package com.atguigu.zcw.mapper;
+
+import com.atguigu.zcw.bean.TParam;
+import com.atguigu.zcw.bean.TParamExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TParamMapper {
+    long countByExample(TParamExample example);
+
+    int deleteByExample(TParamExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TParam record);
+
+    int insertSelective(TParam record);
+
+    List<TParam> selectByExample(TParamExample example);
+
+    TParam selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TParam record, @Param("example") TParamExample example);
+
+    int updateByExample(@Param("record") TParam record, @Param("example") TParamExample example);
+
+    int updateByPrimaryKeySelective(TParam record);
+
+    int updateByPrimaryKey(TParam record);
+}
